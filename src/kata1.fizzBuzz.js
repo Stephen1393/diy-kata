b2fdcf60ffdc4d89e78f145a76c8193e9fed5898
Stephen1393 <stephen.bending20@gmail.com>
2025-11-25 11:52:24 +0000

    Add fizzBuzz solution and tests

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,20 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+
+    if ( number % 3 === 0) {
+        return "Fizz"
+    }
+    if (number % 5 === 0) {
+        return "Buzz"
+    }
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz"
+    }
+    else {
+        return number
+    }
+    };
 
 module.exports = fizzBuzz;
+
+
+
