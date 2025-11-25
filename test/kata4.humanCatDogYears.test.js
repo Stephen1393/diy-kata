ed627e7c3f7ff1d05c00d3d614abab7e984ac22f
Stephen1393 <stephen.bending20@gmail.com>
2025-11-25 15:10:36 +0000

    add humanCatDogYears kata

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,16 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+test("return correct values for 1 human year", () => {
+    expect(humanCatDogYears(1)).toEqual([1,15,15]);
+});
+
+test("return correct values for 2 human years", () => {
+    expect(humanCatDogYears(2)).toEqual([2,24,24])
+});
+
+test("return correct values for 3 human years", () => {
+    expect(humanCatDogYears(3)).toEqual([3,28,29])
+});
+
+});
