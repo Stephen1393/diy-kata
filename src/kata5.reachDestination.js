8f77aac2f60008afa51e4fa2058f02d45290b82e
Stephen1393 <stephen.bending20@gmail.com>
2025-11-26 15:15:41 +0000

    add kata5 reachDestination.

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,16 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
 
-module.exports = reachDestination;
+    const time = distance / speed
+
+const roundedTime = Math.round(time * 4) / 4;
+
+const hours = Math.floor(roundedTime);         
+const minutes = Math.round((roundedTime - hours) * 60)
+
+    const result = "I should be there in " + hours + " hours " + minutes + " minutes.";
+
+    return result 
+
+};
+
+module.exports = reachDestination
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,14 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    
+    const distance = 44;
+    const speed = 10;
+
+    
+    const result = reachDestination(distance, speed);
+
+    expect(result).toBe("I should be there in 4 hours 30 minutes.");
+  });
 });
