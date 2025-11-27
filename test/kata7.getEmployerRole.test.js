e8ebb5d0e4011577480517f76ff520f1dd3c8a22
Stephen1393 <stephen.bending20@gmail.com>
2025-11-27 15:03:39 +0000

    Add getEmployerRole kata 7.

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,22 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    const jobs = [
+      {
+      name: "Stephen",
+      age: 33,
+      role: "software engineer"
+      },
+
+      {
+      name: "Lee",
+      age:33,
+      role: "Warehouseman"
+    },
+    ]
+    const result = getEmployerRole("Lee", jobs) 
+
+    expect(result).toBe("Warehouseman")
+  });
 });
