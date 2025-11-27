0f339a6103c0f4051e4d7d9fd48a1ef54183cbe7
Stephen1393 <stephen.bending20@gmail.com>
2025-11-27 15:40:23 +0000

    Add joinNames kata6.

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,16 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+    const list = namesObj.map(person => person.name);
+
+    if (list.length === 1) {
+        return list[0]
+    }
+    const butLast = list.slice(0, -1).join(", ")
+    const last = list[list.length -1]
+
+    const result = butLast + " & " + last
+    
+    return result
+}
+
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,12 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const names = [
+      {name: "Stephen"},
+      {name: "Lee"},
+      {name: "Natasha"}
+    ]
+    expect(joinNames(names)).toBe("Stephen, Lee & Natasha");
+  });
 });
